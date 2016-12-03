1097db113183d4a3a109cb1a9b3c4d8ece68086f
Kevin Paul Kalis <kevinpaulkalis@gmail.com>
2016-12-03 22:56:59 +0800

    Add gulp

--- src/angular/js/app.js
+++ src/angular/js/app.js
@@ -1,9 +1,40 @@
 var app = angular.module('swiftList', [
-                'ui.bootstrap'
+                'ui.bootstrap',
+                'ngRoute'
                 ])
                 .controller('HomeController', HomeController)
+                .controller('ListController', ListController)
+                .config(routeConfig)
 ;
 
+function routeConfig($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/list/', {
+            templateUrl: 'templates/homePage.html',
+            controller: 'HomeController'
+        })
+        .when('/list/:listId', {
+            templateUrl: 'templates/listPage.html',
+            controller: 'ListController',
+            resolve: {
+                delay: function ($q, $timeout) {
+                    var delay = q.defer();
+                    $timeout(delay.resolve, 1000);
+                    return delay.response
+                }
+            }
+        })
+        .otherwise({
+          redirectTo: "/"
+        })
+
+        ; // end of routeProvider
+}
+
 function HomeController($scope) {
-    $scope.heading = 'Home Page';
+    $scope.heading = 'Home Page'
+}
+
+function ListController($scope) {
+    $scope.heading = 'List Page';
 }
